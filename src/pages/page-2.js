490860bc69ea5b97296435d864f21f5ff6648384
Fabio Albertin <albertin@gmail.com>
2018-09-17 11:40:03 +0100

    More linting and clean up

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import Link from 'gatsby-link';
 
 const SecondPage = () => (
   <div>
-    <h1>Hi from the second page</h1>
+    <h1>Hello from the second page</h1>
     <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <Link to="/">Go back to the first one</Link>
   </div>
-)
+);
 
-export default SecondPage
+export default SecondPage;
